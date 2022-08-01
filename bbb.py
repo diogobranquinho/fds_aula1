a0a25daa45c211a5e432f7e31261e9212d747e9a
Diogo Branquinho <branquinho@Diogos-MacBook-Pro.local>
2022-08-01 17:35:50 -0300

    Revisão dos códigos-fontes

--- bbb.py
+++ bbb.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return "<p> Bom, bonito e barato</p>"
 
 @app.route("/sobre")
 def sobre():
@@ -16,16 +16,3 @@ def sobre():
 @app.route("/sobre/privacidade")
 def privacidade():
     return "<h4> Privacidade nao existe neste site</h4>"
-
-@app.route("/user/<username>")
-def username(username):
-    cok = make_response("<h2>cookie criado</h2>")
-    cok.set_cookie('username', username)
-    return cok
-
-@app.route("/user2/")
-@app.route("/user2/<username>")
-def username2(username=None):
-    cokusername = request.cookies.get('username')
-    print(cokusername)
-    return render_template('user.html',username=username, cokusername=cokusername)
